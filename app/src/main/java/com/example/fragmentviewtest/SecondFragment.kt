c977cbcb39b11888fec9775b5d1aa3724ac7953e
Pau Enrech <pauenrech94@gmail.com>
2021-06-17 22:27:46 +0200

    handle fragment visibility

--- app/src/main/java/com/example/fragmentviewtest/SecondFragment.kt
+++ app/src/main/java/com/example/fragmentviewtest/SecondFragment.kt
@@ -5,7 +5,9 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.commit
 import com.example.fragmentviewtest.databinding.FragmentSecondBinding
 
@@ -13,6 +15,28 @@ class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
     private val binding get() = _binding!!
+
+    private val onBackStackChangedListener = FragmentManager.OnBackStackChangedListener {
+        Log.d(
+            SecondFragment::class.java.simpleName,
+            "lastFragment: ${parentFragmentManager.fragments.lastOrNull()}"
+        )
+        parentFragmentManager.fragments.lastOrNull()?.let {
+            Toast.makeText(
+                requireContext(),
+                "Fragment visible ${this == it}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun isFragmentVisible(actualFragment: Fragment?) =
+        this == actualFragment
+
+    override fun setMenuVisibility(menuVisible: Boolean) {
+        super.setMenuVisibility(menuVisible)
+        Log.d(SecondFragment::class.java.simpleName, "Menu visible $menuVisible")
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,11 +49,13 @@ class SecondFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        this.parentFragmentManager.addOnBackStackChangedListener(onBackStackChangedListener)
         initListener()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        this.parentFragmentManager.removeOnBackStackChangedListener(onBackStackChangedListener)
         _binding = null
     }
 
